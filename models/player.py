1364fa3bb8493ee52ac49bc58f40b6c0c03b2055
Lywald <56117568+Lywald@users.noreply.github.com>
2025-01-15 19:48:57 +0100

    docstring

--- models/player.py
+++ models/player.py
@@ -2,6 +2,30 @@ from datetime import date, datetime
 
 
 class Player:
+    """
+    A class to represent a player in a tournament.
+    Attributes:
+    ----------
+    last_name : str
+        The last name of the player.
+    first_name : str
+        The first name of the player.
+    birth_date : date
+        The birth date of the player.
+    national_id : str
+        The national ID of the player.
+    total_points : int, optional
+        The total points accumulated by the player (default is 0).
+    tournament_points : int, optional
+        The points accumulated by the player in the current tournament (default is 0).
+    Methods:
+    -------
+    to_dict():
+        Converts player information to a dictionary.
+    __repr__():
+        Returns a string representation of the player.
+    """
+
     def __init__(self, last_name: str, first_name: str,
                  birth_date: date, national_id: str, total_points: int = 0, tournament_points: int = 0):
         self.last_name = last_name
